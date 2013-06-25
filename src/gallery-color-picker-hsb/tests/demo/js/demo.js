8d0eefcd1785f24d745bc1d62482b2d4883982d8
kenjiru <kenjiru.ro@gmail.com>
2013-06-26 00:30:23 +0200

    Render directly on page.

--- src/gallery-color-picker-hsb/tests/demo/js/demo.js
+++ src/gallery-color-picker-hsb/tests/demo/js/demo.js
@@ -1,8 +1,7 @@
 YUI().use('node', 'gallery-color-picker-hsb', function(Y){
     console.log('Demo gallery-color-picker-hsb.');
 
-    var placeHolder = Y.one('#colorPicker'),
-        colorPicker = new Y.ColorPickerHsb();
+    var colorPicker = new Y.ColorPickerHsb();
 
-    colorPicker.render(placeHolder);
+    colorPicker.render();
 });
