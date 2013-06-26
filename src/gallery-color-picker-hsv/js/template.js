42546cd115dce5dc8a504952b4ae7e260e9a33b1
kenjiru <kenjiru.ro@gmail.com>
2013-06-26 15:42:17 +0200

    Refactored the class and id attributes of the value elements.

--- src/gallery-color-picker-hsv/js/template.js
+++ src/gallery-color-picker-hsv/js/template.js
@@ -7,31 +7,31 @@ var COLOR_PICKER_HSV_TEMPLATE = "" +
     "</div>" +
     "<div class='values'>" +
     "   <div class='selected-color'></div>" +
-    "   <div class='value'>" +
+    "   <div class='value rgb'>" +
     "       <label for='r'>R</label>" +
     "       <input id='r'/>" +
     "   </div>" +
-    "   <div class='value'>" +
-    "       <label for='g'>G</label>" +
-    "       <input id='g'/>" +
-    "   </div>" +
-    "   <div class='value'>" +
-    "       <label for='b'>B</label>" +
-    "       <input id='b'/>" +
-    "   </div>" +
-    "   <div class='value'>" +
+    "   <div class='value hsv'>" +
     "       <label for='h'>H</label>" +
     "       <input id='h'/>" +
     "   </div>" +
-    "   <div class='value'>" +
+    "   <div class='value rgb'>" +
+    "       <label for='g'>G</label>" +
+    "       <input id='g'/>" +
+    "   </div>" +
+    "   <div class='value hsv'>" +
     "       <label for='s'>S</label>" +
     "       <input id='s'/>" +
     "   </div>" +
-    "   <div class='value'>" +
+    "   <div class='value rgb'>" +
+    "       <label for='b'>B</label>" +
+    "       <input id='b'/>" +
+    "   </div>" +
+    "   <div class='value hsv'>" +
     "       <label for='v'>V</label>" +
     "       <input id='v'/>" +
     "   </div>" +
-    "   <div class='value'>" +
+    "   <div class='value hex'>" +
     "       <label for='hex'>#</label>" +
     "       <input id='hex'/>" +
     "   </div>" +
