efa4439234969eca20a73d5fcf0e16bb7e0d0215
kenjiru <kenjiru.ro@gmail.com>
2013-06-26 15:51:25 +0200

    Refactor the control of the color selectors Calculate the values for HSV.

--- src/gallery-color-picker-hsv/js/color-picker-hsv.js
+++ src/gallery-color-picker-hsv/js/color-picker-hsv.js
@@ -4,6 +4,12 @@ Y.ColorPickerHsv = Y.Base.create('colorPickerHsv', Y.Widget, [], {
     _colorSelector : null,
     _hueControl : null,
     _hueSelector : null,
+    _r : null,
+    _g : null,
+    _b : null,
+    _h : null,
+    _s : null,
+    _v : null,
 
     initializer : function(config) {
         console.log('ColorPickerHsv initialized!');
@@ -19,45 +25,81 @@ Y.ColorPickerHsv = Y.Base.create('colorPickerHsv', Y.Widget, [], {
 
         this._hueControl = contentBox.one('.hue-control');
         this._hueSelector = contentBox.one('.hue-selector');
+
+        this._r = contentBox.one('#r');
+        this._g = contentBox.one('#g');
+        this._b = contentBox.one('#b');
+
+        this._h = contentBox.one('#h');
+        this._s = contentBox.one('#s');
+        this._v = contentBox.one('#v');
     },
 
     bindUI : function() {
-        this._colorControl.on('mousedown', this._moveColorSelector, this);
-        this._colorControl.on('mousemove', this._moveColorSelector, this);
+        this._colorControl.on('mousedown', this._changeColor, this);
+        this._colorControl.on('mousemove', this._changeColor, this);
 
-        this._hueControl.on('mousedown', this._moveHueSelector, this);
-        this._hueControl.on('mousemove', this._moveHueSelector, this);
+        this._hueControl.on('mousedown', this._changeHue, this);
+        this._hueControl.on('mousemove', this._changeHue, this);
     },
 
-    _moveColorSelector: function(e) {
-        var xy, x, y;
+    _changeColor : function(e) {
+        var controlPosition, point;
 
-        // left click
-        if (e.button == 1) {
-            xy = this._colorControl.getXY();
-            x = e.pageX - xy[0];
-            y = e.pageY - xy[1];
+        // check for left click pressed
+        if (e.button !== 1)
+            return;
 
-            console.log(x + ', ' + y);
+        controlPosition = this._colorControl.getXY();
+        point = {
+            x: e.pageX - controlPosition[0],
+            y: e.pageY - controlPosition[1]
+        };
 
-            this._colorSelector.setStyles({
-                'left' : x - 8,
-                'top' : y - 8
-            })
-        }
+        console.log(point.x + ', ' + point.y);
+
+        this._moveColorSelector(point);
+        this._setSaturationAndValue(point);
     },
 
-    _moveHueSelector : function(e) {
-        var controlY, mouseY, y;
+    _changeHue : function(e) {
+        var controlY, y;
 
         // left click
-        if (e.button == 1) {
-            controlY = this._hueControl.getY(),
-            y = e.pageY - controlY,
+        if (e.button !== 1)
+            return;
 
-            console.log(y);
+        controlY = this._hueControl.getY();
+        y = e.pageY - controlY;
 
-            this._hueSelector.setStyle('top', y - 8);
-        }
+        console.log(y);
+
+        this._moveHueSelector(y);
+        this._setHue(y);
+    },
+
+    _setSaturationAndValue : function(point) {
+        var s = parseInt(100*(Math.max(0,Math.min(150, point.x)))/150, 10),
+            v = parseInt(100*(150 - Math.max(0,Math.min(150, point.y)))/150, 10);
+
+        this._s.set('value', s);
+        this._v.set('value', v);
+    },
+
+    _setHue : function(y) {
+        var h = parseInt(360*(150 - Math.max(0,Math.min(150,(y))))/150, 10);
+
+        this._h.set('value', h);
+    },
+
+    _moveColorSelector : function(point) {
+        this._colorSelector.setStyles({
+            'left' : point.x - 8,
+            'top' : point.y - 8
+        });
+    },
+
+    _moveHueSelector : function(y) {
+        this._hueSelector.setStyle('top', y - 8);
     }
 });
