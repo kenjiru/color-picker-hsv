1a93c51957b4259990d4cc4af14a68a9a9aa7960
kenjiru <kenjiru.ro@gmail.com>
2013-07-01 18:07:46 +0200

    Updated the demo files to test the 'colorChange' event.

--- src/gallery-color-picker-hsv/tests/demo/js/demo.js
+++ src/gallery-color-picker-hsv/tests/demo/js/demo.js
@@ -2,8 +2,12 @@ YUI().use('node', 'gallery-color-picker-hsv', function(Y){
     console.log('Demo gallery-color-picker-hsv.');
 
     var colorPicker = new Y.ColorPickerHsv({
-        color : 'f00'
-    });
+            color : 'f00'
+        }),
+        colorInput = Y.one('#colorInput');
 
     colorPicker.render();
+    colorPicker.on('colorPickerHsv:colorChanged', function(ev) {
+        colorInput.set('value', ev.color);
+    });
 });
